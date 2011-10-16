efaa9ce3a37ee926ec1586895336b4e2379e5324
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-10-16 02:01:52 -0400

    mm: Map most files to use export.h instead of module.h
    The files changed within are only using the EXPORT_SYMBOL
    macro variants.  They are not using core modular infrastructure
    and hence don't need module.h but only the export.h header.
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>
    
    Conflicts:
    	mm/shmem.c
    
    Change-Id: I963cc89ab8d91dd2c50aadab99c7834fbde9145d

--- mm/shmem.c
+++ mm/shmem.c
@@ -27,7 +27,7 @@
 #include <linux/pagemap.h>
 #include <linux/file.h>
 #include <linux/mm.h>
-#include <linux/module.h>
+#include <linux/export.h>
 #include <linux/percpu_counter.h>
 #include <linux/swap.h>
 
