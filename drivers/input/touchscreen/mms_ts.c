d238963d5374f8752db78bb438dace0829a37f2b
yank555-lu <yank555.lu@gmail.com>
2013-07-13 16:06:49 +0200

    Touch Wake : bump to v1.1 (Yank555.lu)
    - fix git merge derp in drivers/sensor/cm36651.c
    - fix random misbehaving related to powerkey / proxy
    - slight code cleanup
    - optimized using likely / unlikely
    - added precise debugging prints (enabled by #define)

--- drivers/input/touchscreen/mms_ts.c
+++ drivers/input/touchscreen/mms_ts.c
@@ -2995,7 +2995,6 @@ static void mms_ts_early_suspend(struct early_suspend *h)
   info = container_of(h, struct mms_ts_info, early_suspend);
   mms_ts_suspend(&info->client->dev);
 #endif
-
 }
 
 static void mms_ts_late_resume(struct early_suspend *h)
@@ -3012,7 +3011,7 @@ static void mms_ts_late_resume(struct early_suspend *h)
 static struct mms_ts_info * touchwake_data;
 void touchscreen_disable(void)
 {
-  if (touchwake_data != NULL) 
+  if (likely(touchwake_data != NULL))
     mms_ts_suspend(&touchwake_data->client->dev);
 
     return;
@@ -3021,6 +3020,7 @@ EXPORT_SYMBOL(touchscreen_disable);
 
 void touchscreen_enable(void)
 {
+  if (likely(touchwake_data != NULL))
     mms_ts_resume(&touchwake_data->client->dev);
 
     return;
