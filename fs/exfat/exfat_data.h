e3d65f84c58310d673d48087906b2538c474b861
andip71 <andreasp@gmx.de>
2014-08-07 05:06:25 +0200

    fs: Update exFAT to version 1.2.7 (Samsung OSRC from KK sources)

--- fs/exfat/exfat_data.h
+++ fs/exfat/exfat_data.h
@@ -31,8 +31,8 @@ extern "C" {
 #define FAT_CACHE_HASH_SIZE     64
 #define BUF_CACHE_SIZE          256
 #define BUF_CACHE_HASH_SIZE     64
-// #define DEFAULT_CODEPAGE        437
-// #define DEFAULT_IOCHARSET       "utf8"
+#define DEFAULT_CODEPAGE        437
+#define DEFAULT_IOCHARSET       "utf8"
 #ifdef __cplusplus
 }
 #endif
