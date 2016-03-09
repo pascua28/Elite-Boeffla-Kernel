a456b683227974e016bd431e99f6c86e7427c82f
andip71 <andreasp@gmx.de>
2016-03-09 16:17:48 +0100

    Boeffla ramfs: Remove kernel driven auto root
    Rooting is a science meanwhile, so many times this did not work anymore.
    Instead of trying to fix it, it is now removed completely.

--- ramdisk_boeffla/fs/sbin/boeffla-init.sh
+++ ramdisk_boeffla/fs/sbin/boeffla-init.sh
@@ -162,43 +162,6 @@
 	echo 0 > /sys/module/binder/parameters/debug_mask
 	echo 0 > /sys/module/xt_qtaguid/parameters/debug_mask
 
-# Auto root support
-	if [ -f $SD_PATH/autoroot ]; then
-
-		echo $(date) Auto root is enabled >> $BOEFFLA_LOGFILE
-
-		mount -o remount,rw -t ext4 $SYSTEM_DEVICE /system
-
-		/sbin/busybox mkdir /system/bin/.ext
-		/sbin/busybox cp /res/misc/su /system/xbin/su
-		/sbin/busybox cp /res/misc/su /system/xbin/daemonsu
-		/sbin/busybox cp /res/misc/su /system/bin/.ext/.su
-		/sbin/busybox cp /res/misc/install-recovery.sh /system/etc/install-recovery.sh
-		/sbin/busybox echo /system/etc/.installed_su_daemon
-		
-		/sbin/busybox chown 0.0 /system/bin/.ext
-		/sbin/busybox chmod 0777 /system/bin/.ext
-		/sbin/busybox chown 0.0 /system/xbin/su
-		/sbin/busybox chmod 6755 /system/xbin/su
-		/sbin/busybox chown 0.0 /system/xbin/daemonsu
-		/sbin/busybox chmod 6755 /system/xbin/daemonsu
-		/sbin/busybox chown 0.0 /system/bin/.ext/.su
-		/sbin/busybox chmod 6755 /system/bin/.ext/.su
-		/sbin/busybox chown 0.0 /system/etc/install-recovery.sh
-		/sbin/busybox chmod 0755 /system/etc/install-recovery.sh
-		/sbin/busybox chown 0.0 /system/etc/.installed_su_daemon
-		/sbin/busybox chmod 0644 /system/etc/.installed_su_daemon
-
-		/system/bin/sh /system/etc/install-recovery.sh
-
-		/sbin/busybox sync
-		
-		mount -o remount,ro -t ext4 $SYSTEM_DEVICE /system
-		echo $(date) Auto root: su installed >> $BOEFFLA_LOGFILE
-
-		rm $SD_PATH/autoroot
-	fi
-
 # EFS backup
 	EFS_BACKUP_INT="$BOEFFLA_DATA_PATH/efs.tar.gz"
 	EFS_BACKUP_EXT="/storage/extSdCard/efs.tar.gz"
