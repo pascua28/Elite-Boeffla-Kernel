3a7af3ca6ff7d4a76f11667500a25dd1b1e6ad37
andip71 <andreasp@gmx.de>
2016-06-13 08:26:17 +0200

    zram_drv: Default zRam devices to 4 instead of 1

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -53,7 +53,7 @@ static const char *default_compressor = "lzo";
 #define ALLOC_ERROR_LOG_RATE_MS 1000
 
 /* Module params (documentation at end) */
-static unsigned int num_devices = 1;
+static unsigned int num_devices = 4;
 
 #define ZRAM_ATTR_RO(name)						\
 static ssize_t zram_attr_##name##_show(struct device *d,		\
