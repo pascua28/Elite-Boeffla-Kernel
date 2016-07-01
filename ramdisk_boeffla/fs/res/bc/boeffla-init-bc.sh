f763f6024e9cb0031029f5d3224469bb97aa6806
andip71 <andreasp@gmx.de>
2016-07-01 14:29:14 +0200

    Boeffla ramfs: Change to app based initialization

--- ramdisk_boeffla/fs/res/bc/boeffla-init-bc.sh
+++ ramdisk_boeffla/fs/res/bc/boeffla-init-bc.sh
@@ -1,6 +1,8 @@
 #!/system/bin/sh
 
 # *****************************
+# BC-based initialization
+#
 # i9300 Cyanogenmod 13.0 version
 #
 # V0.1
@@ -54,6 +56,11 @@
 	/sbin/busybox grep ro.build.version /system/build.prop >> $BOEFFLA_LOGFILE
 	echo "=========================" >> $BOEFFLA_LOGFILE
 
+# set busybox selinux labels
+	mount -o rw,remount rootfs /
+	chcon u:object_r:toolbox_exec:s0 /sbin/busybox
+	mount -o ro,remount rootfs /
+
 # remove any obsolete Boeffla-Config V2 startconfig done file
 	/sbin/busybox rm -f $BOEFFLA_STARTCONFIG_DONE
 
@@ -97,14 +104,9 @@
 		echo $(date) init.d script handling by kernel disabled >> $BOEFFLA_LOGFILE
 	fi
 
-# Now wait for the rom to finish booting up
-# (by checking for the android acore process)
-	echo $(date) Checking for Rom boot trigger... >> $BOEFFLA_LOGFILE
-	while ! /sbin/busybox pgrep com.android.systemui ; do
-	  /sbin/busybox sleep 1
-	done
-	echo $(date) Rom boot trigger detected, waiting a few more seconds... >> $BOEFFLA_LOGFILE
-	/sbin/busybox sleep 15
+# Wait for some time to ensure rom is fully initialized
+	echo $(date) Waiting a few more seconds... >> $BOEFFLA_LOGFILE
+	/sbin/busybox sleep 10
 
 # Play sound for Boeffla-Sound compatibility
 	echo $(date) Initialize sound system... >> $BOEFFLA_LOGFILE
@@ -194,11 +196,12 @@
 		echo $(date) "Doze disabled" >> $BOEFFLA_LOGFILE
 	fi
 
-# SELinux part 1 - only reporting what will be done
-	if [ ! -f $PERMISSIVE_ENABLER ]; then
-		echo $(date) "SELinux: enforcing" >> $BOEFFLA_LOGFILE
-	else
+# disable SELinux if configured
+	if [ -f $PERMISSIVE_ENABLER ]; then
+		echo "0" > /sys/fs/selinux/enforce
 		echo $(date) "SELinux: permissive" >> $BOEFFLA_LOGFILE
+	else
+		echo $(date) "SELinux: enforcing" >> $BOEFFLA_LOGFILE
 	fi
 
 # Finished
@@ -206,8 +209,3 @@
 	echo $(date) "Loaded startconfig was:" >> $BOEFFLA_LOGFILE
 	cat $BOEFFLA_STARTCONFIG >> $BOEFFLA_LOGFILE
 	echo $(date) End of kernel startup logfile >> $BOEFFLA_LOGFILE
-
-# SELinux part 2 - actual handling as very last thing (switching it to on will terminate this script)
-	if [ ! -f $PERMISSIVE_ENABLER ]; then
-		echo "1" > /sys/fs/selinux/enforce
-	fi
