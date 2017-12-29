cccbc4e9af03f93f14b115fbc2b18a2e89680fc0
Ярик <acroreiser@gmail.com>
2017-12-29 18:35:29 +0200

    mm: fix shmem_swp_set crash
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- mm/shmem.c
+++ mm/shmem.c
@@ -394,10 +394,6 @@ static void shmem_swp_set(struct shmem_inode_info *info, swp_entry_t *entry, uns
 
 	entry->val = value;
 	info->swapped += incdec;
-	if ((unsigned long)(entry - info->i_direct) >= SHMEM_NR_DIRECT) {
-		struct page *page = kmap_atomic_to_page(entry);
-		set_page_private(page, page_private(page) + incdec);
-	}
 }
 
 /**
