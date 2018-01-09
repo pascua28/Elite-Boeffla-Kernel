f7ec43fff2ed8b08d54a1c889b60a806d4f456a3
pascua28 <pascua.samuel.14@gmail.com>
2018-01-09 14:22:35 +0800

    cpufreq: set 1.6GHz max as default for T0 boards

--- arch/arm/mach-exynos/cpufreq.c
+++ arch/arm/mach-exynos/cpufreq.c
@@ -728,8 +728,13 @@ static int exynos_cpufreq_cpu_init(struct cpufreq_policy *policy)
 	ret = cpufreq_frequency_table_cpuinfo(policy, exynos_info->freq_table);
 	
 	/* Set default startup frq. */
+#ifdef CONFIG_MACH_T0
+	policy->max = 1600000;
+	policy->min = 100000;
+#else
 	policy->max = 1400000;
 	policy->min = 100000;
+#endif
 
 	if (ret)
 		return ret;
