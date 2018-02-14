fab8a52990d7554bb89494078e942535041c00c2
Ярик <acroreiser@gmail.com>
2018-02-14 21:54:09 +0200

    power: disable l2_hsic wakelock
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -373,6 +373,10 @@ EXPORT_SYMBOL_GPL(device_set_wakeup_enable);
  */
 static void wakeup_source_activate(struct wakeup_source *ws)
 {
+	
+	if (!strcmp(ws->name, "l2_hsic")) 
+		return;
+	
 	ws->active = true;
 	ws->active_count++;
 	ws->timer_expires = jiffies;
