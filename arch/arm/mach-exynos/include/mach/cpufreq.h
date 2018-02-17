cb96469ff7d5607a4a7b0744e3827008feca58c8
Samuel Pascua <pascua.samuel.14@gmail.com>
2018-02-17 21:47:01 +0800

    Revert "boeffla_sound: implement incall boost"
    This reverts commit 87ba0e4000ce5846877be39949ce7209897a7aee.

--- arch/arm/mach-exynos/include/mach/cpufreq.h
+++ arch/arm/mach-exynos/include/mach/cpufreq.h
@@ -72,12 +72,10 @@ enum cpufreq_lock_ID {
 	 */
 	DVFS_LOCK_ID_QOS_DMA_LATENCY,
 	DVFS_LOCK_ID_END,
-	DVFS_LOCK_ID_INCALL,
 };
 
 int exynos_cpufreq_get_level(unsigned int freq,
 			unsigned int *level);
-int exynos_cpufreq_get_level_ret(unsigned int freq);
 int exynos_find_cpufreq_level_by_volt(unsigned int arm_volt,
 			unsigned int *level);
 int exynos_cpufreq_lock(unsigned int nId,
