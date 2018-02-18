4d9c0dd944fca62b02eef7479ff7d4af45c72d3a
Samuel Pascua <pascua.samuel.14@gmail.com>
2018-02-18 12:23:41 +0800

    Revert "cpufreq: set 200MHz as minimum frequency for T0 boards"
    This reverts commit a713a56348f022fedd816f67b4e97614243a8f35.

--- arch/arm/mach-exynos/cpufreq.c
+++ arch/arm/mach-exynos/cpufreq.c
@@ -730,7 +730,7 @@ static int exynos_cpufreq_cpu_init(struct cpufreq_policy *policy)
 	/* Set default startup frq. */
 #ifdef CONFIG_MACH_T0
 	policy->max = 1600000;
-	policy->min = 200000;
+	policy->min = 100000;
 #else
 	policy->max = 1400000;
 	policy->min = 100000;
