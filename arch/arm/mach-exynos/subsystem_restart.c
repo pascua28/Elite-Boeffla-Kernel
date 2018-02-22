5b87af27b09bd94df045c35eff68dc2529e32d3e
Samuel Pascua <pascua.samuel.14@gmail.com>
2018-02-22 09:38:34 +0800

    fix build error

--- arch/arm/mach-exynos/subsystem_restart.c
+++ arch/arm/mach-exynos/subsystem_restart.c
@@ -228,11 +228,13 @@ int subsystem_restart(const char *subsys_name)
 
 	subsys->ongoing = true;
 
+#ifdef CONFIG_SEC_DEBUG
 	/* check debug level */
 	if (!sec_debug_level.uint_val) {
 		/* debug level is low, set mdm_dump to Zero */
 		mdm_dump = 0;
 	}
+#endif
 
 	data = kzalloc(sizeof(struct restart_wq_data), GFP_KERNEL);
 	if (!data) {
