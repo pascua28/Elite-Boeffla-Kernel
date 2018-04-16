2c5aa306a07adefc5996078679d6cf4c61d5db04
Samuel Pascua <pascua.samuel.14@gmail.com>
2018-04-16 19:04:33 +0800

    Revert "bcmdhd: minor change from N7100XXUFNI1"
    This reverts commit ad7deded61600f59f145aa511746d647a574ec69.

--- drivers/net/wireless/bcmdhd/dhd_custom_sec.c
+++ drivers/net/wireless/bcmdhd/dhd_custom_sec.c
@@ -65,7 +65,6 @@ const struct cntry_locales_custom translate_custom_table[] = {
 	{"CZ", "CZ", 1},
 	{"DE", "DE", 3},
 	{"DK", "DK", 1},
-	{"DZ", "GB", 1},
 	{"EE", "EE", 1},
 	{"ES", "ES", 1},
 	{"FI", "FI", 1},
