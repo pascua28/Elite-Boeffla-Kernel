d3d0c8dfb52d69e2e9f7389be200a0dab8fb4f7a
Samuel Pascua <pascua.samuel.14@gmail.com>
2018-05-14 23:26:03 +0800

    msm_hotplug: fix io is busy

--- arch/arm/hotplug/msm_hotplug.c
+++ arch/arm/hotplug/msm_hotplug.c
@@ -154,12 +154,15 @@ static inline cputime64_t get_cpu_idle_time_jiffy(unsigned int cpu,
 	return (cputime64_t)jiffies_to_usecs(idle_time);
 }
 
-static inline cputime64_t get_cpu_idle_time(unsigned int cpu, cputime64_t *wall)
+static inline cputime64_t get_cpu_idle_time(unsigned int cpu, cputime64_t *wall, int io_busy)
 {
-       u64 idle_time = get_cpu_idle_time_us(cpu, wall);
+       u64 idle_time = get_cpu_idle_time_us(cpu, io_busy ? wall: NULL);
 
        if (idle_time == -1ULL)
                return get_cpu_idle_time_jiffy(cpu, wall);
+
+	else if (!io_busy)
+		idle_time += get_cpu_iowait_time_us(cpu, wall);
 
        return idle_time;
 }
@@ -179,7 +182,7 @@ static int update_average_load(unsigned int cpu)
 	if (ret)
 		return -EINVAL;
 
-	cur_idle_time = get_cpu_idle_time(cpu, &cur_wall_time);
+	cur_idle_time = get_cpu_idle_time(cpu, &cur_wall_time, io_is_busy);
 
 	wall_time = (unsigned int) (cur_wall_time - pcpu->prev_cpu_wall);
 	pcpu->prev_cpu_wall = cur_wall_time;
