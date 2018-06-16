698fc3f57704ad9b093a2b85808a0702d639fca0
Samuel Pascua <pascua.samuel.14@gmail.com>
2018-06-16 17:52:46 +0800

    mm: adjust dirty writeback for IO boost

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -61,7 +61,7 @@ static inline long sync_writeback_pages(unsigned long dirtied)
 /*
  * Start background writeback (via writeback threads) at this percentage
  */
-int dirty_background_ratio = 20;
+int dirty_background_ratio = 2;
 
 /*
  * dirty_background_bytes starts at 0 (disabled) so that it is a function of
@@ -78,7 +78,7 @@ int vm_highmem_is_dirtyable;
 /*
  * The generator of dirty data starts writeback at this percentage
  */
-int vm_dirty_ratio = 50;
+int vm_dirty_ratio = 5;
 
 /*
  * vm_dirty_bytes starts at 0 (disabled) so that it is a function of
@@ -89,12 +89,12 @@ unsigned long vm_dirty_bytes;
 /*
  * The interval between `kupdate'-style writebacks
  */
-unsigned int dirty_writeback_interval = 5 * 100; /* centiseconds */
+unsigned int dirty_writeback_interval = 3 * 100; /* centiseconds */
 
 /*
  * The longest time for which data is allowed to remain dirty
  */
-unsigned int dirty_expire_interval = 30 * 100; /* centiseconds */
+unsigned int dirty_expire_interval = 50 * 100; /* centiseconds */
 
 /*
  * Flag that makes the machine dump writes/reads and block dirtyings.
