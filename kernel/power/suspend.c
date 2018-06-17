5ed90b9f6dc79443da19a4870564cd1a726f99e7
Samuel Pascua <pascua.samuel.14@gmail.com>
2018-06-17 09:16:20 +0800

    disable sync on suspend

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -381,9 +381,9 @@ int enter_state(suspend_state_t state)
 	if (state == PM_SUSPEND_FREEZE)
 		freeze_begin();
 
-	printk(KERN_INFO "PM: Syncing filesystems ... ");
-	sys_sync();
-	printk("done.\n");
+//	printk(KERN_INFO "PM: Syncing filesystems ... ");
+//	sys_sync();
+//	printk("done.\n");
 
 	pr_debug("PM: Preparing system for %s sleep\n", pm_states[state]);
 	error = suspend_prepare(state);
